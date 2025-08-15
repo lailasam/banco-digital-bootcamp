9f80dd16fd4eb4b27dec6a68c35107370e425bee
lailasam <samaralailaa@gmail.com>
2025-08-15 16:28:04 -0300

    excecoes adicionadas

--- src/br/com/bancodigital/service/GerenciadorClientes.java
+++ src/br/com/bancodigital/service/GerenciadorClientes.java
@@ -1,5 +1,9 @@
 package br.com.bancodigital.service;
 
+import br.com.bancodigital.exceptions.CamposNulosException;
+import br.com.bancodigital.exceptions.ClienteJaExisteException;
+import br.com.bancodigital.exceptions.CpfInvalidoException;
+import br.com.bancodigital.model.Cliente;
 import br.com.bancodigital.repository.IRepositorioCliente;
 import br.com.bancodigital.repository.RepositorioClientes;
 
@@ -9,7 +13,15 @@ private IRepositorioCliente repositorioCliente;
 public GerenciadorClientes(){
     this.repositorioCliente = RepositorioClientes.getInstance();
 }
-public void cadastrarCliente(String cpf, String nome, String email, String telefone, String endereco){
-   repositorioCliente.adicionar(new Cliente(cpf, nome, email, telefone, endereco));
+public void cadastrarCliente(String cpf, String nome, String email, String telefone, String endereco) throws CamposNulosException, ClienteJaExisteException, CpfInvalidoException{
+    if(cpf == null || nome == null || email == null || telefone == null || endereco == null) {
+        throw new CamposNulosException("Nenhum campo pode ser nulo.");
+    }
+    else if (repositorioCliente.listarClientes().containsKey(cpf)) {
+        throw new ClienteJaExisteException("Cliente com CPF " + cpf + " ja cadastrado.");
+    }
+    else{
+       repositorioCliente.adicionar(new Cliente(cpf, nome, email, telefone, endereco)); 
+    } 
 }
 }
