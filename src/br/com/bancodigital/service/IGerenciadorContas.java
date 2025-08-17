bfa58fc9c40755f6f99dfe6406310c690821d8f2
lailasam <samaralailaa@gmail.com>
2025-08-17 13:49:28 -0300

    add calcular rendimento da conta poupanca

--- src/br/com/bancodigital/service/IGerenciadorContas.java
+++ src/br/com/bancodigital/service/IGerenciadorContas.java
@@ -2,13 +2,15 @@ package br.com.bancodigital.service;
 import br.com.bancodigital.exceptions.CaracteresInvalidosException;
 import br.com.bancodigital.exceptions.ContaNaoExisteException;
 import br.com.bancodigital.exceptions.CpfInvalidoException;
+import br.com.bancodigital.exceptions.RepositorioVazioException;
+import br.com.bancodigital.exceptions.SaldoInsuficienteException;
 
 public interface IGerenciadorContas {
     void criarConta(int numero, int agencia, double saldo, String cpfTitular) throws CaracteresInvalidosException, CpfInvalidoException;
     void excluirConta(int numeroConta) throws CaracteresInvalidosException, ContaNaoExisteException;
-    void depositar(int numeroConta, double valor) throws CaracteresInvalidosException;
-    void sacar(int numeroConta, double valor);
-    void transferir(int numeroContaOrigem, int numeroContaDestino, double valor);
-    double exibirSaldo(int numeroConta);
-    void exibirExtrato(int numeroConta);
+    void depositar(int numeroConta, double valor) throws CaracteresInvalidosException, ContaNaoExisteException;
+    void sacar(int numeroConta, double valor) throws CaracteresInvalidosException, SaldoInsuficienteException, ContaNaoExisteException;
+    void transferir(int numeroContaOrigem, int numeroContaDestino, double valor) throws CaracteresInvalidosException, ContaNaoExisteException, SaldoInsuficienteException;
+    double exibirSaldo(int numeroConta) throws ContaNaoExisteException, CaracteresInvalidosException;
+    void exibirExtrato(int numeroConta) throws ContaNaoExisteException, RepositorioVazioException, CaracteresInvalidosException;
 }
