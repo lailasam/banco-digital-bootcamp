8e97af660200e6c3869cfb5f2b7011f3a8cb38ec
lailasam <samaralailaa@gmail.com>
2025-08-16 21:45:59 -0300

    add classe extrato

--- src/br/com/bancodigital/model/Extrato.java
+++ src/br/com/bancodigital/model/Extrato.java
@@ -1,45 +1,25 @@
 package br.com.bancodigital.model;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Extrato {
-   private int numeroConta;
-   private int agencia;
    private double saldo;
-   private String titular;
-   private String tipoConta;
    private LocalDateTime dataTransacao;
    private String operacao;
 
-   public int getNumeroConta() {
-    return numeroConta;
-   }
-   public void setNumeroConta(int numeroConta) {
-    this.numeroConta = numeroConta;
-   }
-   public int getAgencia() {
-    return agencia;
-   }
-   public void setAgencia(int agencia) {
-    this.agencia = agencia;
-   }
+    public Extrato(double saldo, LocalDateTime dataTransacao, String operacao) {
+     this.saldo = saldo;
+     this.dataTransacao = dataTransacao;
+     this.operacao = operacao;
+    }
+
    public double getSaldo() {
     return saldo;
    }
    public void setSaldo(double saldo) {
     this.saldo = saldo;
    }
-   public String getTitular() {
-    return titular;
-   }
-   public void setTitular(String titular) {
-    this.titular = titular;
-   }
-   public String getTipoConta() {
-    return tipoConta;
-   }
-   public void setTipoConta(String tipoConta) {
-    this.tipoConta = tipoConta;
-   }
+   
    public LocalDateTime getDataTransacao() {
     return dataTransacao;
    }
@@ -52,5 +32,14 @@ public class Extrato {
    public void setOperacao(String operacao) {
     this.operacao = operacao;
    }
-
-}
+   @Override
+   public String toString() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy");
+    return "Data da transacao:" 
+    + dataTransacao.format(formatter) 
+    + ", Operacao: " 
+    + operacao 
+    + ", Saldo: " 
+    + saldo;
+   }
+   }
--- src/br/com/bancodigital/repository/RepositorioExtrato.java
+++ src/br/com/bancodigital/repository/RepositorioExtrato.java
@@ -1,5 +1,7 @@
 package br.com.bancodigital.repository;
 
+import br.com.bancodigital.model.Extrato;
+
 public class RepositorioExtrato extends RepositorioContas<Extrato> {
     public RepositorioExtrato() {
         super();
